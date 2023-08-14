21acd7e8852d1c82e23207c06ad855549134444c
Remkhe <135029590+Remkhe@users.noreply.github.com>
2023-08-14 18:36:36 +0700

    Add files via upload

---
+++ Задача про маркетплейсы.OrderTest.java
@@ -0,0 +1,96 @@
+package ru.inno.market.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderTest {
+
+    @Test
+    void getItems() {
+    }
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void addItem() {
+    }
+
+    @Test
+    void applyDiscount() {
+    }
+
+    @Test
+    void getClient() {
+    }
+
+    @Test
+    void getCart() {
+    }
+
+    @Test
+    void getTotalPrice() {
+    }
+
+    @Test
+    void isDiscountApplied() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void testGetItems() {
+    }
+
+    @Test
+    void testGetId() {
+    }
+
+    @Test
+    void testAddItem() {
+    }
+
+    @Test
+    void testApplyDiscount() {
+    }
+
+    @Test
+    void testGetClient() {
+    }
+
+    @Test
+    void testGetCart() {
+    }
+
+    @Test
+    void testGetTotalPrice() {
+    }
+
+    @Test
+    void testIsDiscountApplied() {
+    }
+
+    @Test
+    void testEquals1() {
+    }
+
+    @Test
+    void testHashCode1() {
+    }
+
+    @Test
+    void testToString1() {
+    }
+}
