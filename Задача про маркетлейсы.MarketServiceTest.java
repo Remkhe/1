4ac000eab985ab764c3c5945b62b21eed3a73787
Remkhe <135029590+Remkhe@users.noreply.github.com>
2023-08-14 18:37:33 +0700

    Add files via upload

---
+++ Задача про маркетлейсы.MarketServiceTest.java
@@ -0,0 +1,40 @@
+package ru.inno.market.core;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MarketServiceTest {
+
+    @org.junit.jupiter.api.Test
+    void createOrderFor() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addItemToOrder() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void applyDiscountForOrder() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getOrderInfo() {
+    }
+
+    @Test
+    void testCreateOrderFor() {
+    }
+
+    @Test
+    void testAddItemToOrder() {
+    }
+
+    @Test
+    void testApplyDiscountForOrder() {
+    }
+
+    @Test
+    void testGetOrderInfo() {
+    }
+}
